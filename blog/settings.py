dbd2bfe709caf5982134daffd92f37f5039d9c35
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-07-06 16:47:19 +0530

    Bc kya ho raha hai zindagi and code ke sath

--- blog/settings.py
+++ blog/settings.py
@@ -25,14 +25,19 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ['SECRET_KEY']
+
 SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['localhost','writingsbykaur.herokuapp.com']
+ALLOWED_HOSTS = ['writingsbykaur.herokuapp.com']
+
+
+
+# ALLOWED_HOSTS = ['localhost','writingsbykaur.herokuapp.com']
 
 
 # Application definition
@@ -130,6 +135,19 @@ DATABASES['default'].update(db_from_env)
 
 
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jayesh.bidani@gmail.com' #my gmail username
+EMAIL_HOST_PASSWORD = 'hackathon' #my gmail password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Jayesh <jayesh.bidani@gmail.com>"
+
+
+ADMINS = [('jayesh', EMAIL_HOST_USER)]
+MANAGERS = ADMINS
+
+
+
 ######change site id=8 for local use ########
 SITE_ID = 1
 
@@ -158,7 +176,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
