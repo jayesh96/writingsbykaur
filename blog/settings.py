c5377662cd989357e08e32b66f56222c7baafc27
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-07-06 15:59:28 +0530

    Error pages

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','writingsbykaur.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -212,12 +212,6 @@ SOCIALACCOUNT_PROVIDERS = {
         'VERSION': 'v2.4',
     }
 }
-
-
-
-
-
-
 
 
 
