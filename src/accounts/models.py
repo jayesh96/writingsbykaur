1fe46a5f3193808e4761905f74668a3ab763a82a
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-07-09 19:15:07 +0530

    Added Edit

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Contact(models.Model):
+	pass
