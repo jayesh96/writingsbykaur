a5e83ab1a610779ae7ac2d5514182b249e484a18
Jayesh Bidani <jayesh.bidani@gmail.com>
2017-07-10 13:22:47 +0530

    portfolio edit

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -9,4 +9,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Contact(models.Model):
-	pass
+	name = models.CharField(blank=False, null=False, max_length=200)
+	email = models.EmailField(blank=False, null=False)
+	subject = models.CharField(blank=False, null=False)
+	message = models.TextField(blank=False, null=False)
+
+	def __unicode__(self):
+		return self.subject
+
+	def __str__(self):
+		return self.subject
+
